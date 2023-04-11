769bb26360c822e127d4124a3c93ba8e3e072f00
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-11 15:15:35 +0200

    feat: add profileIngredient to NutrientProfileController

--- app/app/Http/Controllers/NutrientProfileController.php
+++ app/app/Http/Controllers/NutrientProfileController.php
@@ -7,18 +7,56 @@ use App\Models\Ingredient;
 use App\Models\Meal;
 use App\Models\FoodList;
 use App\Models\Unit;
+use App\Models\RdiProfile;
+use Illuminate\Support\Facades\DB;
 
 class NutrientProfileController extends Controller
 {
-    public function profileIngredient(Ingredient $ingredient) {
+    /**
+     *  Computes a NutrientProfile for 100g of the specified Ingredient using
+     *  the specified RdiProfile.
+     */
+    public static function profileIngredient(Ingredient $ingredient, $rdiProfileID, $ingredient_mass_in_grams=100) {
+        if (Ingredient::where('id', $ingredient->id)->doesntExist()) return [];
+        if (RdiProfile::where('id', $rdiProfileID)->doesntExist()) return [];
+
+        $query = "
+        select
+          nutrients.name as nutrient,
+        round((:ingredient_mass_in_g * ingredient_nutrients.amount_per_100g / 100), 2) as amount,
+          units.name as unit,
+        round((:ingredient_mass_in_g * ingredient_nutrients.amount_per_100g / nullif(rdi_profile_nutrients.rdi, 0)), 1) as pdv
+        from ingredient_nutrients
+        inner join nutrients
+          on nutrients.id
+          = ingredient_nutrients.nutrient_id  
+        inner join units
+          on units.id
+          = nutrients.unit_id
+        inner join rdi_profile_nutrients
+          on rdi_profile_nutrients.rdi_profile_id
+          = :rdi_profile_id
+          and rdi_profile_nutrients.nutrient_id
+          = ingredient_nutrients.nutrient_id
+        where ingredient_nutrients.ingredient_id=:ingredient_id
+        order by nutrients.id;
+        ";
+
+        $result = DB::select($query, [
+            'ingredient_mass_in_g' => $ingredient_mass_in_grams,
+            'rdi_profile_id' => $rdiProfileID,
+            'ingredient_id' => $ingredient->id
+        ]);
+
+        return $result;
+
+    }
+
+    public static function profileMeal(Meal $meal) {
         return;
     }
 
-    public function profileMeal(Meal $meal) {
-        return;
-    }
-
-    public function profileFoodList(FoodList $foodList) {
+    public static function profileFoodList(FoodList $foodList) {
         return;
     }
 }
