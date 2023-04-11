794828d3ed1b6c776b27646ccad6d39e69865112
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-11 15:15:45 +0200

    fix: fully seed database

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-          StandardReferenceSeeder::class
+          StandardReferenceSeeder::class,
+          MealSeeder::class,
+            FoodListSeeder::class,
+            RdiProfileSeeder::class
         ]);
     }
 }
