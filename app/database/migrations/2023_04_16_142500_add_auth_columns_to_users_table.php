0a13d3c982a18b87446d159be248e85f3bd0b10c
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-16 16:26:44 +0200

    feat: add auth-related columns to users table

---
+++ app/database/migrations/2023_04_16_142500_add_auth_columns_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_free_tier')->default(true);
+            $table->boolean('is_full_tier')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_free_tier');
+            $table->dropColumn('is_full_tier');
+        });
+    }
+};
