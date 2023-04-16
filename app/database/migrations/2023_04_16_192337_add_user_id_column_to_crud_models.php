29d68dba05762aee2bda7e443c85ef18840f4b5c
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-16 21:27:23 +0200

    feat: add user_id to CRUD model tables

---
+++ app/database/migrations/2023_04_16_192337_add_user_id_column_to_crud_models.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+        Schema::table('meals', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+        Schema::table('food_lists', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+        Schema::table('rdi_profiles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+        Schema::table('meals', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+        Schema::table('food_lists', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+        Schema::table('rdi_profiles', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
