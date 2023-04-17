2b5b4e0bf97097c33f6d50ba7600875ba5b8f0b5
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-17 20:33:51 +0200

    feat: add some button/link components

--- app/resources/js/Components/PrimaryLinkButton.vue
+++ app/resources/js/Components/PrimaryLinkButton.vue
@@ -1,17 +1,18 @@
 <script setup>
-defineProps({
-  type: {
+import { Link } from '@inertiajs/vue3';
+const props = defineProps({
+  method: {
     type: String,
-    default: 'submit',
-  },
-});
+    default: "get"
+  }
+})
 </script>
 
 <template>
-  <button
-    :type="type"
+  <Link
     class="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-400 focus:bg-blue-400 active:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2"
+    :method="method"
   >
-    <slot />
-  </button>
+  <slot />
+  </Link>
   </template>
