7b23cf750a682a65b6faff1b98c440c9a2ff5108
Elijan Mastnak <ejmastnak@gmail.com>
2023-04-17 08:40:48 +0200

    fix: make ingredients.user_id nullable

--- app/database/migrations/2023_04_16_192337_add_user_id_column_to_crud_models.php
+++ app/database/migrations/2023_04_16_192337_add_user_id_column_to_crud_models.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('ingredients', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
         Schema::table('meals', function (Blueprint $table) {
