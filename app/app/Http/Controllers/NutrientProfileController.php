ce9f09a54a9acfcb8b72fd99a53b97489d0a46f0
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-07 21:35:06 -0400

    feat: permit Meal nutrient profile as assoc. array
    Adds an optional parameter to permit returning a Meal's nutrient profile
    as an associative array/symbol table whose keys are nutrient ids. This
    is convenient for saving a Meal as an Ingredient.

--- app/app/Http/Controllers/NutrientProfileController.php
+++ app/app/Http/Controllers/NutrientProfileController.php
@@ -101,7 +101,7 @@ class NutrientProfileController extends Controller
         return $nutrientProfiles;
     }
 
-    public static function profileMeal($mealID, $intakeGuidelineID=1) {
+    public static function profileMeal($mealID, $intakeGuidelineID=1, $return_as_symbol_table=false) {
         if (Meal::where('id', $mealID)->doesntExist()) return [];
         if (IntakeGuideline::where('id', $intakeGuidelineID)->doesntExist()) return [];
 
@@ -140,7 +140,16 @@ class NutrientProfileController extends Controller
             'intake_guideline_id' => $intakeGuidelineID
         ]);
 
-        return $result;
+        if ($return_as_symbol_table) {
+            // Create a symbol table mapping nutrient ids to results
+            $symbol_table = [];
+            foreach ($result as $nutrient) {
+                $symbol_table[$nutrient->nutrient_id] = $nutrient;
+            }
+            return $symbol_table;
+        } else {
+            return $result;  // just return an array of results
+        }
     }
 
     /**
