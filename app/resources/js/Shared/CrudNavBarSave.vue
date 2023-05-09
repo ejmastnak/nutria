970c6b20660d2207f22f370d58e59f31c164b843
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-09 15:32:40 -0400

    feat: add nav bar component for Save As Ingredient

---
+++ app/resources/js/Shared/CrudNavBarSave.vue
@@ -0,0 +1,32 @@
+<script setup>
+import CrudNavBarButton from '@/Shared/CrudNavBarButton.vue'
+import { ArchiveBoxArrowDownIcon } from '@heroicons/vue/24/outline'
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: "Save"
+  },
+  enabled: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['wasClicked'])
+
+</script>
+
+<template>
+  <CrudNavBarButton
+    :enabled="enabled"
+    :text="text"
+    @wasClicked="emit('wasClicked')"
+  >
+    <template v-slot:icon>
+      <ArchiveBoxArrowDownIcon class="h-5 w-5" />
+    </template>
+  </CrudNavBarButton>
+</template>
+
+
