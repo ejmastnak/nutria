cb7f6b35b701bcd2ac0d5a6516802a0656fcfffd
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-11 10:42:29 -0400

    feat: make CrudNavBar responsive on mobile layout

--- app/resources/js/Shared/CrudNavBar.vue
+++ app/resources/js/Shared/CrudNavBar.vue
@@ -1,6 +1,41 @@
+<script setup>
+import { Bars4Icon } from '@heroicons/vue/24/outline'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+
+function closePopover(close) { close() }
+</script>
+
 <!-- A navigation bar with Edit/Delete/etc links for use on CRUD model pages -->
 <template>
-  <header class="flex items-center border border-gray-300 rounded-xl">
-    <slot></slot>
+  <!-- Desktop nav bar -->
+  <header class="hidden sm:flex items-center border border-gray-300 rounded-xl">
+    <slot name="desktop-items"></slot>
   </header>
+
+  <!-- Mobile nav bar -->
+  <header class="flex sm:hidden items-center border border-gray-300 rounded-xl">
+
+    <!-- Icons on left that are always displayed in mobile layout -->
+    <div class="flex mr-auto">
+      <slot name="mobile-displayed"></slot>
+    </div>
+
+    <!-- Popover with hamburger button and mobile menu items -->
+    <Popover class="relative">
+      <!-- Hamburger button for displaying full mobile menu -->
+      <PopoverButton class="px-4 py-2 rounded-xl overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-700">
+        <Bars4Icon class="w-6 h-6 text-gray-500" />
+      </PopoverButton>
+      <!-- Mobile menu items -->
+      <PopoverPanel v-slot="{ close }" class="absolute right-0 z-10 p-1 border border-gray-200 shadow-sm rounded-xl bg-white focus:outline-none focus:ring-1 focus:ring-blue-700">
+        <div class="flex flex-col">
+          <slot name="mobile-items"></slot>
+        </div>
+      </PopoverPanel>
+    </Popover>
+
+  </header>
+
 </template>
+
+
