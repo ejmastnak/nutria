9650d7e778a46c042f4de2a742afb0dab58fc428
Elijan Mastnak <git@ejmastnak.com>
2023-10-12 12:26:42 +0200

    fix: remember how user specified mass of custom units
    Adds (nullable) `custom_unit_amount`, `custom_mass_amount`, and
    `custom_mass_unit_id` columns to Unit, which are needed to remember how
    the user originally specified the mass of a custom unit.

--- database/migrations/2023_03_29_083314_create_units_table.php
+++ database/migrations/2023_03_29_083314_create_units_table.php
@@ -18,6 +18,9 @@ return new class extends Migration {
             $table->integer('seq_num');
             // ingredient_id is added after ingredients table is created
             // meal_id is added after meals table is created
+            $table->double('custom_unit_amount', 10, 4)->nullable();
+            $table->double('custom_mass_amount', 10, 4)->nullable();
+            $table->foreignId('custom_mass_unit_id')->nullable()->references('id')->on('units');
             $table->double('custom_grams', 10, 4)->nullable();
         });
     }
