9562b387911c779f4bbd7bb0c7499f2d117d70c8
Elijan Mastnak <git@ejmastnak.com>
2023-11-16 18:34:23 +0100

    feat: preserve all filters when leaving page

--- resources/js/Pages/Ingredients/Index.vue
+++ resources/js/Pages/Ingredients/Index.vue
@@ -2,7 +2,6 @@
 import fuzzysort from 'fuzzysort'
 import throttle from "lodash/throttle";
 import debounce from "lodash/debounce";
-
 import { ref, watch, onMounted, onBeforeUnmount, computed } from 'vue'
 import { Head, router } from '@inertiajs/vue3'
 import { TrashIcon, PlusCircleIcon, DocumentDuplicateIcon, MagnifyingGlassIcon, XMarkIcon, PencilSquareIcon } from '@heroicons/vue/24/outline'
@@ -64,9 +63,12 @@ const fuzzysortUserOptions = {
   threshold: -10000
 }
 
-const usdaSearchQuery = ref(sessionStorage.getItem('ingredientsIndexUsdaSearchQuery') ?? "")
-const userSearchQuery = ref(sessionStorage.getItem('ingredientsIndexUserSearchQuery') ?? "")
+const usdaSearchQuery = ref("")
+const userSearchQuery = ref("")
 
+const numDisplayedUsdaIngredients = computed(() => {
+  return filteredUsdaIngredients.value.filter(ingredient => shouldDisplayUsdaIngredient(ingredient.obj)).length
+})
 const numDisplayedUserIngredients = computed(() => {
   return filteredUserIngredients.value.filter(ingredient => shouldDisplayUserIngredient(ingredient.obj)).length
 })
@@ -96,6 +98,8 @@ onBeforeUnmount(() => {
   sessionStorage.setItem('ingredientsIndexUsdaSearchQuery', usdaSearchQuery.value);
   sessionStorage.setItem('ingredientsIndexUserSearchQuery', userSearchQuery.value);
   sessionStorage.setItem('ingredientsIndexSelectedTab', selectedTab.value);
+  sessionStorage.setItem('ingredientsIndexUsdaCategeories', JSON.stringify(selectedUsdaCategories.value))
+  sessionStorage.setItem('ingredientsIndexUserCategeories', JSON.stringify(selectedUserCategories.value))
 })
 
 // Preserve search query on manual page reload
@@ -103,6 +107,8 @@ window.onbeforeunload = function() {
   sessionStorage.setItem('ingredientsIndexUsdaSearchQuery', usdaSearchQuery.value);
   sessionStorage.setItem('ingredientsIndexUserSearchQuery', userSearchQuery.value);
   sessionStorage.setItem('ingredientsIndexSelectedTab', selectedTab.value);
+  sessionStorage.setItem('ingredientsIndexUsdaCategeories', JSON.stringify(selectedUsdaCategories.value))
+  sessionStorage.setItem('ingredientsIndexUserCategeories', JSON.stringify(selectedUserCategories.value))
 }
 
 function resetUsdaSearch() {
@@ -134,8 +140,28 @@ function deleteIngredient() {
 
 // Preserve ingredient search from previous visit to this page
 onMounted(() => {
-  if (usdaSearchQuery) { usdaSearch(usdaSearchQuery.value) }
-  if (userSearchQuery) { userSearch(userSearchQuery.value) }
+
+  const storedSelectedUsdaCategories = sessionStorage.getItem('ingredientsIndexUsdaCategeories');
+  if (storedSelectedUsdaCategories) {
+    selectedUsdaCategories.value = JSON.parse(storedSelectedUsdaCategories);
+  }
+
+  const storedSelectedUserCategories = sessionStorage.getItem('ingredientsIndexUserCategeories');
+  if (storedSelectedUserCategories) {
+    selectedUserCategories.value = JSON.parse(storedSelectedUserCategories);
+  }
+
+  const storedUsdaSearchQuery = sessionStorage.getItem('ingredientsIndexUsdaSearchQuery')
+  if (storedUsdaSearchQuery) {
+      usdaSearchQuery.value = storedUsdaSearchQuery
+      usdaSearch(usdaSearchQuery.value)
+  }
+
+  const storedUserSearchQuery = sessionStorage.getItem('ingredientsIndexUserSearchQuery')
+  if (storedUserSearchQuery) {
+      userSearchQuery.value = storedUserSearchQuery
+  }
+  userSearch(userSearchQuery.value)
 })
 
 
@@ -238,6 +264,7 @@ export default {
                   <input
                     type="text"
                     id="usda-search"
+                    placeholder="Type something to see results..."
                     ref="usdaSearchInput"
                     class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 sm:w-64 md:w-80 lg:w-96 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                     v-model="usdaSearchQuery"
@@ -310,6 +337,9 @@ export default {
                 </tr>
               </tbody>
             </table>
+            <p v-show="numDisplayedUsdaIngredients === 0 && usdaSearchQuery !== ''" class="ml-3" >
+                No results found. Try a less restrictive filter or search?
+            </p>
           </section>
         </TabPanel>
 
