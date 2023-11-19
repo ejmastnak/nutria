c68f2bbd1a64670e26613830db097b42566aa70d
Elijan Mastnak <git@ejmastnak.com>
2023-11-19 13:03:56 -0500

    refactor: bring FoodLists/Index up to spec
    Part of larger post-spec refactor; brings FoodLists/Index up to spec.
    Only minor changes, mostly swapping in updated delete dialog and search
    handling.

--- resources/js/Pages/FoodLists/Index.vue
+++ resources/js/Pages/FoodLists/Index.vue
@@ -3,25 +3,33 @@ import fuzzysort from 'fuzzysort'
 import throttle from "lodash/throttle";
 import debounce from "lodash/debounce";
 import { ref, watch, onMounted, onBeforeUnmount } from 'vue'
-import { Head } from '@inertiajs/vue3'
-import { TrashIcon, PlusCircleIcon, DocumentDuplicateIcon, MagnifyingGlassIcon, XMarkIcon, PencilSquareIcon } from '@heroicons/vue/24/outline'
+import { Head, router } from '@inertiajs/vue3'
+import { TrashIcon, MagnifyingGlassIcon, XMarkIcon, PencilSquareIcon } from '@heroicons/vue/24/outline'
 import MyLink from '@/Components/MyLink.vue'
-import H1 from '@/Components/H1ForIndex.vue'
-import PrimaryLinkButton from '@/Components/PrimaryLinkButton.vue'
 import SecondaryButton from '@/Components/SecondaryButton.vue'
-import InputLabel from '@/Components/InputLabel.vue'
-import DeleteDialog from '@/Shared/DeleteDialog.vue'
-import SearchForThingAndGo from '@/Shared/SearchForThingAndGo.vue'
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import H1 from '@/Components/H1ForIndex.vue'
+import DeleteDialog from "@/Components/DeleteDialog.vue";
+import FindAThing from '@/Components/FindAThing.vue'
 
 const props = defineProps({
   food_lists: Array,
   can_create: Boolean
 })
 
-const cloneExistingDialog = ref(null)
+let idToDelete = ref(null)
 const deleteDialog = ref(null)
-const searchInput = ref(null)
+function deleteFoodList() {
+  if (idToDelete.value) {
+    router.delete(route('food-lists.destroy', idToDelete.value), {
+      onSuccess: () => {
+        // Updates filtered food lists after deleting an food list to ensure
+        // the deleted food list disappears from display.
+        search(userSearchQuery.value)
+      }
+    });
+  }
+  idToDelete.value = null
+}
 
 // For fuzzy search over meal names
 const filteredFoodLists = ref([])
@@ -32,39 +40,40 @@ const fuzzysortOptions = {
   threshold: -10000
 }
 
-const search = ref(sessionStorage.getItem('foodListsIndexSearchQuery') ?? "")
+const searchQuery = ref("")
+function search(query) {
+  filteredFoodLists.value = fuzzysort.go(query.trim(), props.food_lists, fuzzysortOptions)
+}
 
-// Preserve food list search from previous visit to this page
-onMounted(() => {
-  if (search) {
-    filteredFoodLists.value = fuzzysort.go(search.value.trim(), props.food_lists, fuzzysortOptions)
-  }
-})
-
-watch(search, throttle(function (value) {
-  filteredFoodLists.value = fuzzysort.go(value.trim(), props.food_lists, fuzzysortOptions)
+watch(searchQuery, throttle(function (value) {
+  search(value)
 }, 300))
 
 // Preserve search query between page visits
 onBeforeUnmount(() => {
-  sessionStorage.setItem('foodListsIndexSearchQuery', search.value);
+  sessionStorage.setItem('foodListsIndexSearchQuery', searchQuery.value);
 })
 
 // Preserve search query on manual page reload
 window.onbeforeunload = function() {
-  sessionStorage.setItem('foodListsIndexSearchQuery', search.value);
+  sessionStorage.setItem('foodListsIndexSearchQuery', searchQuery.value);
 }
 
-function resetSearch() {
-  search.value = ""
-  searchInput.value.focus()
+const searchInputRef = ref(null)
+function clearSearch() {
+  searchQuery.value = ""
+  searchInputRef.value.focus()
 }
 
-// Updates filteredFoodLists after deleting a food list to ensure the meal
-// disappears from display
-function updateFuzzySearchOnDeletion(id) {
-  filteredFoodLists.value = fuzzysort.go(search.value.trim(), props.food_lists, fuzzysortOptions)
-}
+// Preserve food list search from previous visit to this page
+onMounted(() => {
+  const storedSearchQuery = sessionStorage.getItem('foodListsIndexSearchQuery')
+  if (storedSearchQuery) {
+      searchQuery.value = storedSearchQuery
+  }
+  search(searchQuery.value)
+})
+
 
 </script>
 
@@ -76,65 +85,55 @@ export default {
 </script>
 
 <template>
-  <div class="">
+  <div>
     <Head title="Food Lists" />
 
     <!-- Title and new food list top row -->
-    <div class="flex">
-
-      <div class="mr-2 p-1">
-        <H1 text="Food Lists" />
-        <p class="mt-2 w-11/12 4 sm:w-2/3 text-gray-500">
-          Use this page as an overview of food lists you have created.
-        </p>
-      </div>
-
-      <div class="flex flex-col ml-auto w-fit">
-
-        <!-- New food list button -->
-        <PrimaryLinkButton
-          :href="route('food-lists.create')"
-          class="flex ml-auto items-center py-2.0 sm:py-2.5 mt-1 normal-case"
-          :class="{'!bg-blue-200': !can_create}"
-        >
-          <PlusCircleIcon class="w-6 h-6" />
-          <p class="ml-2 font-semibold text-base whitespace-nowrap">New food list</p>
-        </PrimaryLinkButton>
-
-        <!-- Clone food list button -->
-        <SecondaryButton
-          class="flex ml-auto items-center mt-1 normal-case"
-          :class="{'!text-gray-300': !can_create}"
-          @click="cloneExistingDialog.open()"
-        >
-          <DocumentDuplicateIcon class="w-6 h-6" />
-          <p class="ml-2 font-semibold text-base whitespace-nowrap">Clone existing</p>
-        </SecondaryButton>
-
-      </div>
+    <div class="mr-2 p-1">
+      <H1 text="Food Lists" />
+      <p class="mt-2 w-11/12 4 sm:w-2/3 text-gray-500">
+        Use this page as an overview of food lists you have created.
+      </p>
     </div>
 
     <!-- Food list table -->
     <section class="mt-8 border border-gray-200 px-4 py-2 rounded-xl shadow-sm bg-white">
 
       <!-- Input for search -->
-      <div class="px-2 py-4">
+      <div class="px-2 py-4 flex flex-wrap items-end">
+        <div>
+          <label for="food-list-search" class="ml-1 text-sm text-gray-500">
+            Search by food list name
+          </label>
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <MagnifyingGlassIcon class="w-5 h-5 text-gray-500" />
+            </div>
 
-        <label for="food-list-search" class="ml-1 text-sm text-gray-500">
-          Search by food list name
-        </label>
-        <div class="relative">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <MagnifyingGlassIcon class="w-5 h-5 text-gray-500" />
+            <input
+              type="text"
+              id="food-list-search"
+              ref="searchInputRef"
+              class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 sm:w-64 md:w-80 lg:w-96 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              v-model="searchQuery"
+            />
           </div>
+        </div>
 
-          <input
-            type="text"
-            id="food-list-search"
-            ref="searchInput"
-            class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 sm:w-64 md:w-80 lg:w-96 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-            v-model="search"
-          />
+        <!-- Clear search button -->
+        <div class="ml-2">
+          <label for="clear-search" class="sr-only">
+            Clear filter
+          </label>
+          <SecondaryButton
+            type="button"
+            id="clear-search"
+            class="normal-case font-normal !tracking-normal !text-sm !px-2 h-fit ml-2"
+            @click="clearSearch"
+          >
+            <XMarkIcon class="w-5 h-5" />
+            <span class="text-gray-600 font-normal ml-1.5">Clear search</span>
+          </SecondaryButton>
         </div>
       </div>
 
@@ -208,16 +207,12 @@ export default {
 
     </section>
 
-    <SearchForThingAndGo
-      ref="cloneExistingDialog"
-      :things="food_lists"
-      goRoute="food-lists.clone"
-      label="Search for a food list to clone"
-      title="Clone food list"
-      action="Clone"
+    <DeleteDialog
+      ref="deleteDialog"
+      description="food list"
+      @delete="deleteFoodList"
+      @cancel="idToDelete = null"
     />
-
-    <DeleteDialog ref="deleteDialog" deleteRoute="food-lists.destroy" thing="food list" @delete="updateFuzzySearchOnDeletion" />
 
   </div>
 </template>
