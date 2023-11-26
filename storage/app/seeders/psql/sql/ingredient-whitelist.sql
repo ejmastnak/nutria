74eaf11d85b89695f49b9a0422014af38863553e
Elijan Mastnak <git@ejmastnak.com>
2023-11-26 09:18:08 -0500

    refactor: switch from TSV to CSV for ingredient whitelist
    I'm now using a CSV format for the ingredient whitelist. Original
    motivation was serverside PostgreSQL having trouble importing a TSV file
    with \COPY, but CSV is cleaner and more consistent with other seed
    files.

--- storage/app/seeders/psql/sql/ingredient-whitelist.sql
+++ storage/app/seeders/psql/sql/ingredient-whitelist.sql
@@ -2,14 +2,15 @@
 -- Creates and populates temporary ingredient whitelist table
 -- Prerequisite: ingredient whitelist tsv file exists
 -------------------------------------------------------------------------------
-create table tmp_ingredient_whitelist(
-  include text,
+create temporary table tmp_ingredient_whitelist(
+  is_whitelisted text,
   fdc_id integer,
   ingredient_category_name text,
   ingredient_name text
 );
 
-\COPY tmp_ingredient_whitelist (include, fdc_id, ingredient_category_name, ingredient_name) FROM ./csv/ingredient-whitelist.tsv WITH HEADER;
+\COPY tmp_ingredient_whitelist (is_whitelisted, fdc_id, ingredient_category_name, ingredient_name) FROM ./csv/ingredient-whitelist.csv WITH DELIMITER ',' HEADER CSV;
 
-delete from tmp_ingredient_whitelist where include not like 'W';
+-- Only keep whitelisted ingredients
+delete from tmp_ingredient_whitelist where is_whitelisted not like '1';
 -------------------------------------------------------------------------------
