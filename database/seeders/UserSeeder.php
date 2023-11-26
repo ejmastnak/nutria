552c8a1c6dbccdb28a8ed3c62ae042d85e7b9fec
Elijan Mastnak <git@ejmastnak.com>
2023-11-26 13:14:19 -0500

    feat: seed separate users in local and prod

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,34 +16,55 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-        User::updateOrCreate([
-        'name' => 'admin',
-        'username' => 'admin',
-        'email' => 'admin@ejmastnak.com',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-        'is_registered' => true,
-        'is_paying' => false,
-        'is_admin' => true,
-        ]);
 
-        User::updateOrCreate([
-        'name' => 'registered',
-        'username' => 'registered',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-        'is_registered' => true,
-        'is_paying' => false,
-        'is_admin' => false,
-        ]);
+        if (\App::environment('local')) {
+            User::updateOrCreate([
+                'name' => 'admin',
+                'username' => 'admin',
+                'email' => 'admin@ejmastnak.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'is_registered' => true,
+                'is_paying' => false,
+                'is_admin' => true,
+            ]);
+            User::updateOrCreate([
+                'name' => 'registered',
+                'username' => 'registered',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'is_registered' => true,
+                'is_paying' => false,
+                'is_admin' => false,
+            ]);
+            User::updateOrCreate([
+                'name' => 'paying',
+                'username' => 'paying',
+                'email' => 'paying@ejmastnak.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'is_registered' => true,
+                'is_paying' => true,
+                'is_admin' => false,
+            ]);
+        }
 
-        User::updateOrCreate([
-        'name' => 'paying',
-        'username' => 'paying',
-        'email' => 'paying@ejmastnak.com',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-        'is_registered' => true,
-        'is_paying' => true,
-        'is_admin' => false,
-        ]);
-
+        if (\App::environment('production')) {
+            User::updateOrCreate([
+                'name' => 'Elijan Mastnak',
+                'username' => 'ejmastnak',
+                'email' => 'accounts@ejmastnak.com',
+                'password' => '$2y$10$KB7bn6XBlFL9B4P3Fc8RxeUUO8kmT.vS4OjCkDhGKJRgLlSbePiZK',
+                'is_registered' => true,
+                'is_paying' => true,
+                'is_admin' => false,
+            ]);
+            User::updateOrCreate([
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@ejmastnak.com',
+                'password' => '$2y$10$KB7bn6XBlFL9B4P3Fc8RxeUUO8kmT.vS4OjCkDhGKJRgLlSbePiZK',
+                'is_registered' => true,
+                'is_paying' => true,
+                'is_admin' => true,
+            ]);
+        }
     }
 }
