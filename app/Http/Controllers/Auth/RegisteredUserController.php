01ffda84e523c28a3fcfa2a880d8032269ae78d5
Elijan Mastnak <git@ejmastnak.com>
2023-12-07 09:09:01 -0500

    fix: ensure usernames are unique in registration validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'username' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:'.User::class,
             'name' => 'nullable|string|max:255',
             'email' => 'nullable|string|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
