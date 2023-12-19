e21572f39712c2e010c423908dda7a3809faab2d
Elijan Mastnak <git@ejmastnak.com>
2023-12-19 11:10:52 -0500

    style: tweak menu heading

--- resources/js/Layouts/Partials/FoodPopover.vue
+++ resources/js/Layouts/Partials/FoodPopover.vue
@@ -10,7 +10,7 @@ import MyLink from '@/Components/MyLink.vue'
       <PopoverButton
         class="h-16 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
       >
-        <span>Food</span>
+        <span>Foods</span>
         <ChevronDownIcon class="ml-2 -mr-0.5 h-4 w-4" aria-hidden="true" />
       </PopoverButton>
       <PopoverOverlay class="fixed inset-0 bg-black opacity-20 z-50" />
