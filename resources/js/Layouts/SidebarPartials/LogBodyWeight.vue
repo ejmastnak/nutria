12c24f243f7fb777ec88eb425b439767bf908f4d
Elijan Mastnak <git@ejmastnak.com>
2024-01-09 20:58:42 -0500

    feat: add updated BodyWeightRecord logging to SidebarLayout

--- resources/js/Layouts/SidebarPartials/LogBodyWeight.vue
+++ resources/js/Layouts/SidebarPartials/LogBodyWeight.vue
@@ -4,20 +4,37 @@ import { ref } from 'vue'
 import { ScaleIcon } from '@heroicons/vue/24/outline'
 import SidebarButton from './SidebarButton.vue'
 import SidebarIcon from './SidebarIcon.vue'
+import LogBodyWeightRecordDialog from '@/Shared/LogBodyWeightRecordDialog.vue'
+import LogBodyWeightRecordsDialog from '@/Shared/LogBodyWeightRecordsDialog.vue'
+import { bodyWeightRecordsForm } from '@/Shared/store.js'
 
 const props = defineProps({
   units: Array,
 })
 
+const logBodyWeightRecordDialogRef = ref(null)
+const logBodyWeightRecordsDialogRef = ref(null)
+
+function logBodyWeight() {
+  if (bodyWeightRecordsForm.bodyWeightRecords.length >= 1) {
+    logBodyWeightRecordsDialogRef.value.open()
+  } else {
+    logBodyWeightRecordDialogRef.value.open(null)
+  }
+}
+
 </script>
 
 <template>
 
-  <SidebarButton @click="" class="flex">
+  <SidebarButton @click="logBodyWeight" class="flex">
     <SidebarIcon>
       <ScaleIcon />
     </SidebarIcon>
     <p class="ml-1.5">Log body weight</p>
   </SidebarButton>
+
+  <LogBodyWeightRecordDialog :units="units" ref="logBodyWeightRecordDialogRef" />
+  <LogBodyWeightRecordsDialog :units="units" ref="logBodyWeightRecordsDialogRef" />
 
 </template>
